50e47a211eb528f868b854ae14aef3f8218a291b
sunjiabao <202177811@qq.com>
2018-01-02 22:41:14 +0800

    修改了main

--- src/api/VRhangye.js
+++ src/api/VRhangye.js
@@ -1,4 +1,5 @@
 import request from '../utils/request'
+
 //获取社区所有板块分类
 export function bbscategories(query){
     return request({
--- src/view/main.vue
+++ src/view/main.vue
@@ -35,6 +35,7 @@
 		</div>
 	</div>
 </template>
+
 <script>
 	import { bbscategories } from '../api/VRhangye'
 
